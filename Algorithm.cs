b36bd73a7316eda56813922588e057a5e511fc67
Balázs Makszim <b.makszim@hotmail.com>
2020-09-27 21:28:13 +0200

    Add optimization. Continue addition only if still possible to be the highest.

--- Algorithm.cs
+++ Algorithm.cs
@@ -10,23 +10,29 @@ namespace ProjectEuler18
     {
         private int highest = 0;
         private List<int> order = null;
+        private int called = 0;
 
         public int Run(List<List<int>> input, out List<int> order)
         {
             Sum(0, 0, input, 0, new List<int>());
             order = this.order;
+            Console.WriteLine("Called: {0} times", called);
             return highest;
         }
 
         private void Sum(int row, int index, List<List<int>> tree, int sum, List<int> order)
         {
+            called++;
             order.Add(index);
             sum += tree[row][index];
 
             if (row + 1 < tree.Count)
             {
-                Sum(row + 1, index, tree, sum, new List<int>(order));
-                Sum(row + 1, index + 1, tree, sum, new List<int>(order));
+                if (sum + (tree.Count - row - 1) * 99 > highest)
+                {
+                    Sum(row + 1, index, tree, sum, new List<int>(order));
+                    Sum(row + 1, index + 1, tree, sum, new List<int>(order));
+                }
             }
             else
             {

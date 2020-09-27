e02f585e963be5c21727fc5c2a9b7fc5408fb8aa
Balázs Makszim <b.makszim@hotmail.com>
2020-09-27 20:01:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var alg = new Algorithm();
+            Console.WriteLine(alg.Run());
+            Console.Read();
+        }
+    }
+}

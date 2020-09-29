0c4447b162fa1e09989d3e2d7ad59bfbf15f06e9
Balázs Makszim <b.makszim@hotmail.com>
2020-09-29 20:02:49 +0200

    Add path creation for painter

--- AlgorithmQuick.cs
+++ AlgorithmQuick.cs
@@ -10,8 +10,13 @@ namespace ProjectEuler18
     {
         public int Run(List<List<int>> input, out List<int> order)
         {
-            order = new List<int> { 0 };
             var pyramid = input.Select(inner => new List<int>(inner)).ToList();
+            var path = new List<List<int>>();
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                path.Add(new List<int>());
+            }
 
             int row = pyramid.Count - 2;
 
@@ -19,20 +24,35 @@ namespace ProjectEuler18
             {
                 for (int index = 0; index < pyramid[row].Count; index++)
                 {
-                    //if (input[row + 1][index] > input[row + 1][index + 1])
-                    //{
-
-                    //}
-                    //else
-                    //{
-
-                    //}
-                    pyramid[row][index] += Math.Max(pyramid[row + 1][index], pyramid[row + 1][index + 1]);
+                    if (pyramid[row + 1][index] > pyramid[row + 1][index + 1])
+                    {
+                        pyramid[row][index] += pyramid[row + 1][index];
+                        path[row].Add(index);
+                    }
+                    else
+                    {
+                        pyramid[row][index] += pyramid[row + 1][index + 1];
+                        path[row].Add(index + 1);
+                    }
                 }
                 row--;
             }
 
+            order = CreateOrder(path);
             return pyramid[0][0];
+        }
+
+
+        private List<int> CreateOrder(List<List<int>> path)
+        {
+            int index = 0;
+            var order = new List<int> { index };
+            for (int row = 0; row < path.Count - 1; row++)
+            {
+                index = path[row][index];
+                order.Add(index);
+            }
+            return order;
         }
     }
 }
